f8fb740473d9570473465378d18950d9e3c04a78
Abhinav Mishra <abhinav@oozou.com>
2021-07-01 16:45:38 +0700

    Update Readme file

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,18 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".infura").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1", 
       port: 7545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider({ mnemonic: mnemonic, providerOrUrl: `https://rinkeby.infura.io/v3/${infuraKey}` }),
+      network_id: 4,      
+      gas: 5500000,    
+      confirmations: 2,  
+      timeoutBlocks: 200, 
+      skipDryRun: true
     }
-    // rinkeby: {
-    //   provider: () => new HDWalletProvider({ mnemonic: mnemonic, providerOrUrl: `https://rinkeby.infura.io/v3/${infuraKey}` }),
-    //   network_id: 4,      
-    //   gas: 5500000,    
-    //   confirmations: 2,  
-    //   timeoutBlocks: 200, 
-    //   skipDryRun: true
-    // }
   },
   compilers: {
     solc: {
